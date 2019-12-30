36cf277d4eaf4d302581151f2ec476c8881b7646
Nasrin Torabi <nasrin.torabi.git@outlook.com>
2019-12-30 15:22:50 +1030

    First Commit

---
+++ DelegateAndEvents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegateAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
