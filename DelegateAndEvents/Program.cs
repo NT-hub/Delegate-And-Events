5f04b17b7751f16cde70878ae6f8e4010a897498
Nasrin Torabi <nasrin.torabi.git@outlook.com>
2020-01-02 11:54:02 +1030

    Invoked a delegate

--- DelegateAndEvents/Program.cs
+++ DelegateAndEvents/Program.cs
@@ -2,18 +2,29 @@
 
 namespace DelegateAndEvents
 {
+    // Define a delegate
     public delegate void WorkPerformedhandler(int hours, WorkType workType);
     class Program
     {
         static void Main(string[] args)
         {
+            // creating custom delegates and passed in the callback
             WorkPerformedhandler del1 = new WorkPerformedhandler(WorkPerformed1);
             WorkPerformedhandler del2 = new WorkPerformedhandler(WorkPerformed2);
 
             del1(5, WorkType.Golf);
             del2(10, WorkType.GenerateReports);
+
+            DoWork(del2);
+
             Console.Read();
         }
+        static void DoWork(WorkPerformedhandler del)
+        {
+            // invoke a delegate
+            del(5, WorkType.GoToMeetings);
+        }
+
         static void WorkPerformed1(int hours, WorkType workType)
         {
             Console.WriteLine("WorkPerform1 called"+ hours.ToString());
