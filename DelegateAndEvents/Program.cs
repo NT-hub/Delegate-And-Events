280405feb7cdaf9578488e963e879444ebee75ad
Nasrin Torabi <nasrin.torabi.git@outlook.com>
2020-01-02 11:41:29 +1030

    Created the custome delegates

--- DelegateAndEvents/Program.cs
+++ DelegateAndEvents/Program.cs
@@ -2,11 +2,31 @@
 
 namespace DelegateAndEvents
 {
+    public delegate void WorkPerformedhandler(int hours, WorkType workType);
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WorkPerformedhandler del1 = new WorkPerformedhandler(WorkPerformed1);
+            WorkPerformedhandler del2 = new WorkPerformedhandler(WorkPerformed2);
+
+            del1(5, WorkType.Golf);
+            del2(10, WorkType.GenerateReports);
+            Console.Read();
+        }
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerform1 called"+ hours.ToString());
+        }
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkedPerformed2 called"+ hours.ToString());
         }
     }
+        public enum WorkType
+        {
+            GoToMeetings,
+            Golf,
+            GenerateReports
+        }
 }
